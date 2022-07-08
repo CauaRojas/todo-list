f3c4c343064b119be359b904bdbd18e2c75cc44d
Caua Rojas <cauarojas@hotmail.com>
2022-07-08 12:24:04 -0300

    added the reading file function

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,70 @@
-use std::env::args;
+use std::{
+    env::args,
+    fs::File,
+    io::{Read, Write},
+};
 
 struct Todo {
+    id: u32,
     name: String,
     state: bool,
 }
 impl Todo {
-    fn new(name: &str) -> Todo {
+    fn new(name: &str, id: u32) -> Todo {
         Todo {
+            id: id,
             name: String::from(name),
             state: false,
         }
     }
 }
+fn get_file() -> (Vec<Todo>, u32) {
+    let path = ".todos";
+    let mut todos: Vec<Todo> = Vec::new();
+    let mut file: File;
+    let mut buffer: String = String::new();
+    let mut last_id = 0;
+    if !std::path::Path::new(path).exists() {
+        _ = File::create(path)
+            .expect("Error while creating file")
+            .write_all("\n".as_bytes())
+            .expect("Error while writing to file");
+    }
+    file = File::open(path).expect("Error while opening file .todos");
+    file.read_to_string(&mut buffer)
+        .expect("Error while reading todo");
+    for todo in buffer.split("\n") {
+        if todo.trim().len() < 1 {
+            let splited = todo.split("-").collect::<Vec<&str>>();
+            let id = splited[0]
+                .trim()
+                .parse::<u32>()
+                .expect("Error while parsing");
+            let name = splited[1];
+            todos.push(Todo::new(name, id))
+        }
+    }
 
+    match todos.last() {
+        Some(todo) => last_id = todo.id,
+        None => last_id = 0,
+    };
+    (todos, last_id)
+}
+fn save_to_file(todos: & Vec<Todo>){
+
+}
 fn main() {
+    let (mut todos, mut last_id)= get_file();
     for (pos, arg) in args().enumerate() {
         if pos == 0 {
             continue;
         }
-        if arg == "-h" || arg == "--help"{
-            println!("");
+        if arg == "-h" || arg == "--help" {
+            println!("Basic commands: ");
+            println!("\t -n | Adds new todo");
             break;
         }
-        }
+    }
+    save_to_file(&todos);
 }
